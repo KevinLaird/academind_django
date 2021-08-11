eaf750d044a48b57e99a6f067f9b5fe30a8ce58b
Kevin Laird <41528700+KevinLaird@users.noreply.github.com>
2021-08-10 18:37:56 -0700

    Final Product
    Final Product based on this tutorial:
    https://www.youtube.com/watch?v=t7DrJqcUviA&t=12857s

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('meetups/', views.index, name="all-meetups"), #our domain.com/meetups
-	path('meetups/<slug:meetup_slug>', views.meetup_details, name="meetup-details")
+	path('', views.index, name="all-meetups"), #our domain.com/meetups
+	path('<slug:meetup_slug>/success', views.confirm_registration, name="confirm-registration"),
+	path('<slug:meetup_slug>', views.meetup_details, name="meetup-detail"),
+
 ]
